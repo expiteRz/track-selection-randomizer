2bed42a0b1eb58069fa1ba2052f1fd1156793586
expiteRz <none@rz-public.xyz>
2022-10-20 13:06:58 +0900

    me stupid

--- main.go
+++ main.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "math/rand"
-    "os"
-    "strings"
-    "time"
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
 )
 
 func main() {
-    fmt.Printf("Track Selection Randomizer\n\n")
+	fmt.Printf("Track Selection Randomizer\n\n")
 
-    time.Sleep(time.Second)
-    
-    file, err := os.ReadFile("./tracks.txt")
-    if err != nil {
-        println("tracks.txt not exists\nPlease decode Common.bmg from CTGPRC, only take track names out and put it into the same directory with track-selection-randomizer.exe")
-        return
-    }
+	time.Sleep(time.Second)
 
-    s := strings.Split(string(file), "\n")
+	file, err := os.ReadFile("./tracks.txt")
+	if err != nil {
+		println("tracks.txt not exists\nPlease decode Common.bmg from the distribution, only take track names out and put it into the same directory with track-selection-randomizer.exe")
+		return
+	}
 
-    rand.Seed(time.Now().Unix())
+	s := strings.Split(string(file), "\n")
 
-    fmt.Printf("Picked track: %s\n", s[rand.Intn(len(s))])
+	rand.Seed(time.Now().Unix())
 
-    fmt.Println("Press any key to exit the program.")
-    scan := bufio.NewScanner(os.Stdin)
-    scan.Scan()
+	fmt.Printf("Picked track: %s\n", s[rand.Intn(len(s))])
+
+	fmt.Println("Press any key to exit the program.")
+	scan := bufio.NewScanner(os.Stdin)
+	scan.Scan()
 }
